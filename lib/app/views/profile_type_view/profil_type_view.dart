471cae7b0c1e60cda0501779fee4f1b3770d6b6b
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2023-11-16 22:52:07 +0300

    add ✨: profile page

--- lib/app/views/profile_type_view/profil_type_view.dart
+++ lib/app/views/profile_type_view/profil_type_view.dart
@@ -1,51 +1,283 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 import 'package:influencer_app/core/constants/colors_constants/light_theme_constants/light_theme_color_constants.dart';
-import 'package:influencer_app/core/extension/context_extension.dart';
-import 'package:influencer_app/gen/assets.gen.dart';
+
 
 @RoutePage()
-class ProfileTypeView extends StatelessWidget {
+//import 'dart:html';
+
+class ProfileTypeView extends StatefulWidget {
   const ProfileTypeView({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
+  _ProfileTypeViewState createState() => _ProfileTypeViewState();
+}
+
+class _ProfileTypeViewState extends State<ProfileTypeView> {
+
+  int counter = 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      backgroundColor: AppLightColorConstants.bgLight,
-      appBar: AppBar(
-        elevation: 0,
-        centerTitle: true,
-        title: const Text(
-          "TITLE",
-          style: TextStyle(fontSize: 20),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+            counter = counter + 1;
+          });
+        },
+        child: Container(
+          width: 60,
+          height: 60,
+          child: const Icon(
+            Icons.add
+          ),
+          decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: LinearGradient(
+                colors: [AppLightColorConstants.buttonPrimaryColor,Colors.deepPurpleAccent],)),
         ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            // SizedBox(height: context.constHighValue* 19/10,),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(35),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Expanded(
+                flex:5,
+                child:Container(
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [AppLightColorConstants.buttonPrimaryColor,Colors.deepPurpleAccent],
+                    ),
+                  ),
+                  child: const Column(
+                    children: [
+                      SizedBox(height: 110.0,),
+                      CircleAvatar(
+                        radius: 65.0,
+                        backgroundImage: AssetImage('assets/images/png/image_png_profile_theme_two.png'),
+                        backgroundColor: Colors.white,
+                      ),
+                      SizedBox(height: 10.0,),
+                      Text('Erza Scarlet',
+                      style: TextStyle(
+                        color:Colors.white,
+                        fontSize: 20.0,
+                      )),
+                      SizedBox(height: 10.0,),
+                      Text('S Class Mage',
+                      style: TextStyle(
+                        color:Colors.white,
+                        fontSize: 15.0,
+                      ),)
+                  ]
+                  ),
+                ),
               ),
-              width: context.width * .92,
-              child: Image.asset(
-                Assets.images.png.imagePngProfileThemeOne.path,
+
+              Expanded(
+                flex:5,
+                child: Container(
+                  color: Colors.grey[200],
+                  child: Center(
+                      child:Card(
+                          margin: const EdgeInsets.fromLTRB(0.0, 45.0, 0.0, 0.0),
+                        child: Container(
+                          width: 310.0,
+                          height:290.0,
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text("Information",
+                                style: TextStyle(
+                                  fontSize: 17.0,
+                                  fontWeight: FontWeight.w800,
+                                ),),
+                                Divider(color: Colors.grey[300],),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Icon(
+                                      Icons.home,
+                                      color: Colors.blueAccent[400],
+                                      size: 35,
+                                    ),
+                                    const SizedBox(width: 20.0,),
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        const Text("Guild",
+                                          style: TextStyle(
+                                            fontSize: 15.0,
+                                          ),),
+                                        Text("FairyTail, Magnolia",
+                                          style: TextStyle(
+                                            fontSize: 12.0,
+                                            color: Colors.grey[400],
+                                          ),)
+                                      ],
+                                    )
+
+                                  ],
+                                ),
+                                const SizedBox(height: 20.0,),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Icon(
+                                      Icons.auto_awesome,
+                                      color: Colors.yellowAccent[400],
+                                      size: 35,
+                                    ),
+                                    const SizedBox(width: 20.0,),
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        const Text("Magic",
+                                          style: TextStyle(
+                                            fontSize: 15.0,
+                                          ),),
+                                        Text("Spatial & Sword Magic, Telekinesis",
+                                          style: TextStyle(
+                                            fontSize: 12.0,
+                                            color: Colors.grey[400],
+                                          ),)
+                                      ],
+                                    )
+
+                                  ],
+                                ),
+                                const SizedBox(height: 20.0,),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Icon(
+                                      Icons.favorite,
+                                      color: Colors.pinkAccent[400],
+                                      size: 35,
+                                    ),
+                                    const SizedBox(width: 20.0,),
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        const Text("Loves",
+                                          style: TextStyle(
+                                            fontSize: 15.0,
+                                          ),),
+                                        Text("Take a photo",
+                                          style: TextStyle(
+                                            fontSize: 12.0,
+                                            color: Colors.grey[400],
+                                          ),)
+                                      ],
+                                    )
+
+                                  ],
+                                ),
+                                const SizedBox(height: 20.0,),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Icon(
+                                      Icons.people,
+                                      color: Colors.lightGreen[400],
+                                      size: 35,
+                                    ),
+                                    const SizedBox(width: 20.0,),
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        const Text("Team",
+                                          style: TextStyle(
+                                            fontSize: 15.0,
+                                          ),),
+                                        Text("Team Natsu",
+                                          style: TextStyle(
+                                            fontSize: 12.0,
+                                            color: Colors.grey[400],
+                                          ),)
+                                      ],
+                                    )
+
+                                  ],
+                                ),
+                              ],
+                            ),
+                          )
+                        )
+                      )
+                    ),
+                  ),
               ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(35),
-              ),
-              width: context.width * .92,
-              child: Image.asset(
-                Assets.images.png.imagePngProfileThemeOne.path,
-              ),
-            ),
-          ],
-        ),
+
+            ],
+          ),
+          Positioned(
+              top:MediaQuery.of(context).size.height*0.45,
+              left: 20.0,
+              right: 20.0,
+              child: Card(
+                child: Padding(
+                  padding:const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          Text('Battles',
+                          style: TextStyle(
+                            color: Colors.grey[400],
+                            fontSize: 14.0
+                          ),),
+                          const SizedBox(height: 5.0,),
+                          Text("$counter",
+                          style: const TextStyle(
+                            fontSize: 15.0,
+                          ),)
+                        ],
+                      ),
+
+                      Column(
+                      children: [
+                        Text('Last View',
+                          style: TextStyle(
+                              color: Colors.grey[400],
+                              fontSize: 14.0
+                          ),),
+                        const SizedBox(height: 5.0,),
+                        const Text('April 7th',
+                          style: TextStyle(
+                            fontSize: 15.0,
+                          ),)
+                      ]),
+
+                      Column(
+                        children: [
+                          Text('Age',
+                            style: TextStyle(
+                                color: Colors.grey[400],
+                                fontSize: 14.0
+                            ),),
+                          const SizedBox(height: 5.0,),
+                          const Text('19 yrs',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                            ),)
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              )
+          )
+        ],
+
       ),
     );
   }
 }
+
+
